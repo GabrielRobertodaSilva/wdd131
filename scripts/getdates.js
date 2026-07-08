9f2d893460ef24a5d158f87ba13277ecb0fdcf2d
GabrielRobertodaSilva <gabriel.roberto12@hotmail.com>
2026-07-08 14:31:19 -0300

    create web profile page

---
+++ scripts/getdates.js
@@ -0,0 +1,9 @@
+
+const currentYearSpan = document.getElementById("currentyear");
+
+currentYearSpan.textContent = new Date().getFullYear();
+
+
+const lastModifiedParagraph = document.getElementById("lastModified");
+
+lastModifiedParagraph.innerHTML = `Last Modified: ${document.lastModified}`;
